895b30cd091dc633383879208bbfc15dc721f159
Tonny Nguyen <148969983+totodesigns@users.noreply.github.com>
2024-02-16 12:13:24 +0100

    done

--- search_function.js
+++ search_function.js
@@ -1,24 +1,26 @@
 document.addEventListener('keyup',searchbank);
 
 function searchbank() {
-  // Declaring variables
+
   let input, filter, ul, li, a, i, txtValue;
+
+// NADIALIVA ^^^
 
   input = document.getElementById('search');
   filter = input.value.toUpperCase();
   ul = document.getElementById("subjects");
   li = ul.getElementsByTagName('li');
 
-  // Loop through all list items, and hide those who don't match the search query
+// ANDREAS ^^^
+
   for (i = 0; i < li.length; i++) {
 
-    // Get the first anchor element inside the current list item (li[i])
     a = li[i].getElementsByTagName("a")[0];
 
-    // Get the text content of the anchor element, preferring 'textContent' but falling back to 'innerText'
-    txtValue = a.textContent; //|| a.innerText;
+    txtValue = a.textContent;
 
-    // Convert the text content to uppercase and check if it contains the filter string
+// DENNIS ^^^
+
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
     } 
@@ -27,3 +29,5 @@ function searchbank() {
     }
   }
 }
+
+// TONNY ^^^
